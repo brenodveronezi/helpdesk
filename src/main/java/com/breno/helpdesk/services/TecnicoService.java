b5a2f1199d80600889817319d3495b5a04c44782
Breno <brenodveronezi@gmail.com>
2022-01-23 19:20:00 -0300

    addded delete method to tecnico

--- src/main/java/com/breno/helpdesk/services/TecnicoService.java
+++ src/main/java/com/breno/helpdesk/services/TecnicoService.java
@@ -47,6 +47,16 @@ public class TecnicoService {
         return repository.save(oldObj);
     }
 
+    public void delete(Integer id) {
+        Tecnico obj = findById(id);
+
+        if(obj.getChamados().size() > 0){
+            throw new DataIntegrityViolationException("Técnico possuí ordens de serviço e não pode ser deletado!");
+        }else{
+            repository.deleteById(id);
+        }
+    }
+
     private void validaPorCpfEEmail(TecnicoDTO objDTO) {
         Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
         if(obj.isPresent() && !Objects.equals(obj.get().getId(), objDTO.getId())) {
