bd28c0e9a317eb0bb59f90ff0b4a2b8d1c06cfb8
Breno <brenodveronezi@gmail.com>
2022-01-23 18:10:09 -0300

    exceptions for cpf and email already existing

--- src/main/java/com/breno/helpdesk/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/breno/helpdesk/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.breno.helpdesk.resources.exceptions;
 
+import com.breno.helpdesk.services.exceptions.DataIntegrityViolationException;
 import com.breno.helpdesk.services.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +19,14 @@ public class ResourceExceptionHandler {
                 "Object Not Found", ex.getMessage(), request.getRequestURI());
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException ex, HttpServletRequest request){
+        StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
+                "Data Violation", ex.getMessage(), request.getRequestURI());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 
 }
--- src/main/java/com/breno/helpdesk/services/TecnicoService.java
+++ src/main/java/com/breno/helpdesk/services/TecnicoService.java
@@ -1,8 +1,11 @@
 package com.breno.helpdesk.services;
 
+import com.breno.helpdesk.domain.Pessoa;
 import com.breno.helpdesk.domain.Tecnico;
 import com.breno.helpdesk.domain.dtos.TecnicoDTO;
+import com.breno.helpdesk.domain.repositores.PessoaRepository;
 import com.breno.helpdesk.domain.repositores.TecnicoRepository;
+import com.breno.helpdesk.services.exceptions.DataIntegrityViolationException;
 import com.breno.helpdesk.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,6 +18,8 @@ public class TecnicoService {
 
     @Autowired
     private TecnicoRepository repository;
+    @Autowired
+    private PessoaRepository pessoaRepository;
 
     public Tecnico findById(Integer id){
         Optional<Tecnico> obj = repository.findById(id);
@@ -27,7 +32,20 @@ public class TecnicoService {
 
     public Tecnico create(TecnicoDTO objDTO) {
         objDTO.setId(null);
+        validaPorCpfEEmail(objDTO);
         Tecnico newObj = new Tecnico(objDTO);
         return repository.save(newObj);
+    }
+
+    private void validaPorCpfEEmail(TecnicoDTO objDTO) {
+        Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
+        if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+            throw  new DataIntegrityViolationException("CPF já cadastrado no sistema!");
+        }
+
+        obj = pessoaRepository.findByEmail(objDTO.getEmail());
+        if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+            throw  new DataIntegrityViolationException("Email já cadastrado no sistema!");
+        }
     }
 }
